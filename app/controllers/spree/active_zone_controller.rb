ceed96e28bba098f1721e0ec5ec34b12b417b1fd
Matthew Fenelon <matt@fnln.me>
2014-12-02 16:35:16 +0000

    Allow a visitor to change their zone with an order in-progress +
    The order is cleared to avoid checkout differences between zones.

--- app/controllers/spree/active_zone_controller.rb
+++ app/controllers/spree/active_zone_controller.rb
@@ -2,16 +2,27 @@ module Spree
   class ActiveZoneController < Spree::StoreController
     def set
       zone_id = params[:zone]
-
-      if current_order
-        zone = current_order.shipping_zone
-      else
-        zone = Spree::Zone.find_by(id: zone_id)
-      end
+      zone = Spree::Zone.find_by(id: zone_id)
 
       if zone
         SpreeZoned::ActiveZone::Session.current.set(zone)
         SpreeZoned::ActiveCountry.set(cookies, nil)
+
+        if current_order
+          ActiveRecord::Base.transaction do
+            if current_order.bill_address
+              current_order.bill_address = nil
+            end
+
+            if current_order.ship_address
+              current_order.ship_address = nil
+            end
+
+            current_order.empty!
+            current_order.currency = zone.currency
+            current_order.restart_checkout_flow
+          end
+        end
       end
 
       respond_to do |format|
